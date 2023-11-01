68981e8b35599d542aebbe3ae90d2e2bda1431d8
Ranjith <ranjith8796.rk@gmail.com>
2023-11-01 16:02:07 +0530

    Added Procfile

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 class Constants:
 
-    MONGODB_URL="mongodb+srv://ranjith8796:ranjith8796.rk@cluster0.htsbant.mongodb.net/?retryWrites=true&w=majority"
+    MONGODB_URL="mongodb+srv://ranjith8796:ranjith8796.rk@cluster0.htsbant.mongodb.net/?retryWrites=true&w=majority&timeoutMS=300000"
     MONGODB_DB="flask_mongodb"
